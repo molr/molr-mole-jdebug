830dc3b137c2fe9c1e06eb4525898bbd5fcc6a87
Jens Egholm <jensep@gmail.com>
2016-01-26 17:23:31 +0100

    Documented and tested ClassInstantiationListener

--- inspector/src/main/java/cern/jarrace/inspector/jdi/ClassInstantiationListener.java
+++ inspector/src/main/java/cern/jarrace/inspector/jdi/ClassInstantiationListener.java
@@ -7,67 +7,65 @@
 package cern.jarrace.inspector.jdi;
 
 import com.sun.jdi.ClassType;
-import com.sun.jdi.InterfaceType;
 import com.sun.jdi.ReferenceType;
 import com.sun.jdi.event.ClassPrepareEvent;
 import org.jdiscript.handlers.OnClassPrepare;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Consumer;
 
 /**
  * An {@link OnClassPrepare} implementation which keeps track of new implementations of a given class (or interface).
  * Whenever a class that has not been initialised before is added, the callback given in the constructor is called.
+ * This class is not thread safe.
  */
 public class ClassInstantiationListener implements OnClassPrepare {
 
-    private final Consumer<ClassType> implementorCallback;
-    private final Class<?> implementorInterface;
+    private static final Logger LOGGER = LoggerFactory.getLogger(ClassInstantiationListener.class);
 
-    private Set<ClassType> currentImplementations = Collections.emptySet();
-    private InterfaceType implementorType;
+    private final Consumer<ClassType> implementorCallback;
+    private final Class<?> implementorClass;
+    private Set<ClassType> currentImplementations = new HashSet<>();
 
     /**
      * Creates a new listener instructed to search for implementations of the given class. The callback will be
      * called everytime a class or subclass is instantiated that has <i>not</i> been instantiated - i. e. created
      * for the first time.
-     * @param implementorClass The class or interface to search for.
+     *
+     * @param implementorClass    The class or interface to search for.
      * @param implementorCallback A callback to call whenever a new (unique) class instance is created.
      */
     public ClassInstantiationListener(Class<?> implementorClass, Consumer<ClassType> implementorCallback) {
-        this.implementorInterface = implementorClass;
+        this.implementorClass = implementorClass;
         this.implementorCallback = implementorCallback;
     }
 
     @Override
     public void classPrepare(ClassPrepareEvent event) {
-        ReferenceType referenceType = event.referenceType();
-        if (implementorType == null) {
-            if (referenceType.name().equals(implementorInterface.getName())) {
-                implementorType = (InterfaceType) referenceType;
+        final ReferenceType referenceType = event.referenceType();
+        if (referenceType == null) {
+            LOGGER.warn("No reference type defined in class preparation event: {}", event);
+        } else if (referenceType instanceof ClassType) {
+            final ClassType classType = (ClassType) referenceType;
+            if (isClassEquals(classType, implementorClass) && !currentImplementations.contains(classType)) {
+                currentImplementations.add(classType);
+                implementorCallback.accept(classType);
             }
-        } else {
-            Set<ClassType> currentInspectableImplementations = new HashSet<>(implementorType.implementors());
-            Set<ClassType> difference = differenceLeft(currentImplementations, currentInspectableImplementations);
-            currentImplementations = currentInspectableImplementations;
-            difference.forEach(implementorCallback::accept);
         }
     }
 
     /**
-     * Creates a new set which contains all elements which are in the second but not in the first {@link Set}.
-     * @param first The first set of elements.
-     * @param second The second set of elements.
-     * @param <T> The type of element in the sets.
-     * @return A set containing all the elements which exists in the second set, but not in the first set. May be empty.
+     * Tests if the given {@link ClassType} is representing the same class as the given {@link Class} by comparing their
+     * full path names.
+     * @param type The class type to test.
+     * @param clazz The Java class to test.
+     * @return True if the classes are considered the same.
      */
-    static <T> Set<T> differenceLeft(Set<T> first, Set<T> second) {
-        final Set<T> difference = new HashSet<>();
-        for (T elem : second) {
-            if (!first.contains(elem)) {
-                difference.add(elem);
-            }
-        }
-        return Collections.unmodifiableSet(difference);
+    static boolean isClassEquals(ClassType type, Class<?> clazz) {
+        return type.name().equals(clazz.getName());
     }
+
 }
--- inspector/src/test/java/cern/jarrace/inspector/jdi/ClassInstantiationListenerTest.java
+++ inspector/src/test/java/cern/jarrace/inspector/jdi/ClassInstantiationListenerTest.java
@@ -6,13 +6,67 @@
 
 package cern.jarrace.inspector.jdi;
 
+import com.sun.jdi.ClassType;
+import com.sun.jdi.event.ClassPrepareEvent;
+import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Matchers;
 
+import java.util.function.Consumer;
+
+import static cern.jarrace.inspector.jdi.ClassInstantiationListener.isClassEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+@SuppressWarnings("unchecked")
 public class ClassInstantiationListenerTest {
+
+    private static final Class<?> CLASS = ClassInstantiationListenerTest.class;
+    private Consumer mockConsumer;
+    private ClassInstantiationListener listener;
+    private ClassPrepareEvent mockEvent;
+    private ClassType mockType;
+
+    @Before
+    public void setup() {
+        mockConsumer = mock(Consumer.class);
+        mockEvent = mock(ClassPrepareEvent.class);
+        mockType = mock(ClassType.class);
+        when(mockEvent.referenceType()).thenReturn(mockType);
+        when(mockType.name()).thenReturn(CLASS.getName());
+        listener = new ClassInstantiationListener(CLASS, mockConsumer);
+    }
+
+    @Test
+    public void canVerifyIfClassTypeAndClassAreEqual() {
+        assertTrue(isClassEquals(mockType, CLASS));
+    }
+
+    @Test
+    public void canVerifyIfClassTypeAndClassAreDifferent() {
+        when(mockType.name()).thenReturn("another name");
+        assertFalse(isClassEquals(mockType, CLASS));
+    }
 
     @Test
     public void canCallCallback() {
+        listener.classPrepare(mockEvent);
+        verify(mockConsumer).accept(mockType);
+    }
 
+    @Test
+    public void canNotFailIfReferenceTypeIsNull() {
+        when(mockEvent.referenceType()).thenReturn(null);
+        listener.classPrepare(mockEvent);
+        verify(mockConsumer, never()).accept(Matchers.any(ClassType.class));
+    }
+
+    @Test
+    public void canNotCallCallbackIfClassTypeIsNotSameAsClass() {
+        when(mockType.name()).thenReturn("I don't exist");
+        listener.classPrepare(mockEvent);
+        verify(mockConsumer, never()).accept(Matchers.any(ClassType.class));
     }
 
 }
