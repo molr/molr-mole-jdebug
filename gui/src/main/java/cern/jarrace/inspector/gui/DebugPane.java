7ae91dc98b220a8716f0aba1b9e2bf001aaa4896
timartin <tiago.martins.ribeiro@cern.ch>
2016-02-03 14:16:21 +0100

    Changed text format

--- gui/src/main/java/cern/jarrace/inspector/gui/DebugPane.java
+++ gui/src/main/java/cern/jarrace/inspector/gui/DebugPane.java
@@ -17,6 +17,7 @@ import java.util.Arrays;
 /**
  * Implementation {@link BorderPane} that shows the source code in a {@link TextFlow} node and allows for the stepping
  * and termination of the execution.
+ *
  * @author tiagomr
  */
 public class DebugPane extends BorderPane {
@@ -30,7 +31,7 @@ public class DebugPane extends BorderPane {
 
     public DebugPane(String sourceCodeText) {
         super();
-        if(sourceCodeText == null) {
+        if (sourceCodeText == null) {
             throw new IllegalArgumentException("Source code text must not be null");
         }
         initUI();
@@ -39,13 +40,14 @@ public class DebugPane extends BorderPane {
 
     /**
      * Highlights the line given as a parameter
+     *
      * @param lineNumber Line to be highlighted
      */
     public void setCurrentLine(int lineNumber) {
-        if(lineNumber < 1) {
+        if (lineNumber < 1) {
             throw new IllegalArgumentException("Line number must have a positive value");
         }
-        if(lineNumber > textFlow.getChildren().size()) {
+        if (lineNumber > textFlow.getChildren().size()) {
             throw new IllegalArgumentException("Line number must not be bigger than the existent number of lines");
         }
         Text textLine = (Text) textFlow.getChildren().get(currentLine);
@@ -69,7 +71,7 @@ public class DebugPane extends BorderPane {
     }
 
     private void initData(String sourceCodeText) {
-        if(!sourceCodeText.isEmpty()) {
+        if (!sourceCodeText.isEmpty()) {
             Arrays.asList(sourceCodeText.split("\n")).forEach(line -> {
                 Text text = new Text(line + "\n");
                 text.setOnMouseClicked(event -> setCurrentLine(textFlow.getChildren().indexOf(text)));
