ca1bac5f93ae4f6d0e6a6554f3e29c0471d26a36
makemeunsee <makemeunsee@gmail.com>
2016-02-22 12:57:22 +0100

    move common code to abstract. fixes initialization NPE.

--- commons/src/main/java/cern/molr/commons/mole/impl/AbstractMolrRegistry.java
+++ commons/src/main/java/cern/molr/commons/mole/impl/AbstractMolrRegistry.java
@@ -1,7 +1,13 @@
 package cern.molr.commons.mole.impl;
 
+import cern.molr.commons.domain.Mission;
 import cern.molr.commons.mole.MissionsDiscoverer;
 import cern.molr.commons.mole.MolrRegistry;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
 /**
  * Abstract implementation of the {@link MolrRegistry} that searches for {@link cern.molr.commons.domain.Mission}s when
@@ -11,7 +17,29 @@ import cern.molr.commons.mole.MolrRegistry;
  */
 public abstract class AbstractMolrRegistry implements MolrRegistry {
 
+    private final Set<Mission> missions = new HashSet<>();
+
     public AbstractMolrRegistry(MissionsDiscoverer missionsDiscoverer) {
         registerMissions(missionsDiscoverer.availableMissions());
+    }
+
+    @Override
+    public Set<Mission> getMissions() {
+        return new HashSet<>(missions);
+    }
+
+    @Override
+    public Set<Mission> getMissions(Predicate<Mission> predicate) {
+        return missions.stream().filter(predicate).collect(Collectors.toSet());
+    }
+
+    @Override
+    public void registerMission(Mission mission) {
+        this.missions.add(mission);
+    }
+
+    @Override
+    public void registerMissions(Set<Mission> missions) {
+        this.missions.addAll(missions);
     }
 }
--- commons/src/main/java/cern/molr/commons/mole/impl/InMemoryMolrRegistry.java
+++ commons/src/main/java/cern/molr/commons/mole/impl/InMemoryMolrRegistry.java
@@ -4,11 +4,6 @@ import cern.molr.commons.domain.Mission;
 import cern.molr.commons.mole.MissionsDiscoverer;
 import cern.molr.commons.mole.MolrRegistry;
 
-import java.util.HashSet;
-import java.util.Set;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
 /**
  * Implementation of {@link MolrRegistry} that stores {@link Mission}s in memory only.
  * This implementation does not provide state persistence features, meaning that all the registrations are lost on
@@ -18,33 +13,11 @@ import java.util.stream.Collectors;
  */
 public class InMemoryMolrRegistry extends AbstractMolrRegistry {
 
-    private final Set<Mission> missions = new HashSet<>();
-
     public InMemoryMolrRegistry() {
         super(new ClasspathAnnotatedMissionDiscoverer());
     }
 
     public InMemoryMolrRegistry(MissionsDiscoverer missionsDiscoverer) {
         super(missionsDiscoverer);
-    }
-
-    @Override
-    public Set<Mission> getMissions() {
-        return new HashSet<>(missions);
-    }
-
-    @Override
-    public Set<Mission> getMissions(Predicate<Mission> predicate) {
-        return missions.stream().filter(predicate).collect(Collectors.toSet());
-    }
-
-    @Override
-    public void registerMission(Mission mission) {
-        this.missions.add(mission);
-    }
-
-    @Override
-    public void registerMissions(Set<Mission> missions) {
-        this.missions.addAll(missions);
     }
 }
