efbd6c13dca2041246375b78ddfde47a9029be7b
makemeunsee <G1thubL3r0n3>
2016-02-26 18:39:18 +0100

    Molr Swing wrapper notifies component of window closing

--- gui/src/main/java/cern/molr/inspector/gui/SwingUtils.java
+++ gui/src/main/java/cern/molr/inspector/gui/SwingUtils.java
@@ -10,9 +10,12 @@ import javafx.application.Platform;
 import javafx.embed.swing.JFXPanel;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.stage.WindowEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.swing.*;
-import java.util.Optional;
+import java.awt.event.WindowAdapter;
 import java.util.function.Supplier;
 
 /**
@@ -25,23 +28,32 @@ public final class SwingUtils {
 
     // TODO tests have to be done
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(SwingUtils.class);
+
     /**
      * Swing compatibility builder embedding a {@link Parent} inside a {@link JFrame}.
      *
      * @param supplier a way to build a {@link Parent} component
      * @return a {@link JFrame} with a freshly built {@link Parent} as its root component
      */
-    public static JFrame getJFXFrame(Supplier<Optional<Parent>> supplier) {
+    public static <T extends Parent> JFrame getJFXFrame(Supplier<T> supplier) {
         JFrame frame = new JFrame();
         frame.setSize(500, 900);
         JFXPanel fxPanel = new JFXPanel();
         frame.add(fxPanel);
 
         Platform.runLater(() -> {
-            Parent parent = supplier.get()
-                    .orElseThrow(() -> new IllegalStateException("No parent to build JFrame from"));
+            Parent parent = supplier.get();
             Scene scene = new Scene(parent);
             fxPanel.setScene(scene);
+
+            frame.addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(java.awt.event.WindowEvent event) {
+                    LOGGER.info("windowClosing", event);
+                    Platform.runLater(() -> parent.fireEvent(new WindowEvent(scene.getWindow(), WindowEvent.WINDOW_CLOSE_REQUEST)));
+                }
+            });
         });
 
         return frame;
